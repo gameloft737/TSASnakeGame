5c4a941f39511ff411ac662965172b0e361db4c2
gameloft737 <atharva.doke737@gmail.com>
2025-11-18 18:09:29 -0500

    lasers

--- Assets/LaserAttack.cs
+++ Assets/LaserAttack.cs
@@ -7,22 +7,21 @@ public class LaserAttack : Attack
     [SerializeField] private LineRenderer rightLineRenderer;
     [SerializeField] private ParticleSystem leftParticles;
     [SerializeField] private ParticleSystem rightParticles;
-    
+
     [Header("Laser Settings")]
     [SerializeField] private float laserDistance = 8f;
     [SerializeField] private float laserWidth = 0.1f;
     [SerializeField] private LayerMask ignoreMask;
     [SerializeField] private float laserDuration = 5f;
-    
-    
+
     [Header("References")]
     [SerializeField] private Transform leftLaserOrigin;
     [SerializeField] private Transform rightLaserOrigin;
     [SerializeField] private AttackManager attackManager;
-    
+
     private bool isLaserActive = false;
     private float laserActiveTimer = 0f;
-    
+
     private AppleEnemy leftTarget;
     private AppleEnemy rightTarget;
     private bool isDamagingLeft = false;
@@ -32,7 +31,7 @@ public class LaserAttack : Attack
     {
         SetupLineRenderer(leftLineRenderer);
         SetupLineRenderer(rightLineRenderer);
-        
+
         if (leftParticles != null) leftParticles.Stop();
         if (rightParticles != null) rightParticles.Stop();
     }
@@ -45,7 +44,7 @@ public class LaserAttack : Attack
             lr.enabled = false;
             lr.startWidth = laserWidth;
             lr.endWidth = laserWidth;
-            lr.useWorldSpace = true;
+            lr.useWorldSpace = false; // now local to origin
         }
     }
 
@@ -54,7 +53,7 @@ public class LaserAttack : Attack
         if (isLaserActive)
         {
             laserActiveTimer += Time.deltaTime;
-            
+
             if (laserActiveTimer >= laserDuration)
             {
                 DeactivateLaser();
@@ -70,7 +69,6 @@ public class LaserAttack : Attack
     protected override void Use()
     {
         if (leftLaserOrigin == null || rightLaserOrigin == null) return;
-
         ActivateLaser();
     }
 
@@ -78,57 +76,59 @@ public class LaserAttack : Attack
     {
         isLaserActive = true;
         laserActiveTimer = 0f;
-        
+
         if (leftLineRenderer != null) leftLineRenderer.enabled = true;
         if (rightLineRenderer != null) rightLineRenderer.enabled = true;
-        
+
         Debug.Log("Lasers activated!");
+        leftParticles.Play();
+        rightParticles.Play();
     }
 
     private void DeactivateLaser()
     {
         isLaserActive = false;
         laserActiveTimer = 0f;
-        
+
         if (leftLineRenderer != null) leftLineRenderer.enabled = false;
         if (rightLineRenderer != null) rightLineRenderer.enabled = false;
-        
-        StopDamaging(ref leftTarget, ref isDamagingLeft, leftParticles);
-        StopDamaging(ref rightTarget, ref isDamagingRight, rightParticles);
-        
+
+        StopDamaging(ref leftTarget, ref isDamagingLeft);
+        StopDamaging(ref rightTarget, ref isDamagingRight);
+        leftParticles.Stop();
+        rightParticles.Stop();
         Debug.Log("Lasers deactivated!");
     }
 
     private void UpdateLaser(Transform origin, LineRenderer lineRenderer, ParticleSystem particles, ref AppleEnemy currentTarget, ref bool isDamaging)
     {
         if (origin == null || lineRenderer == null) return;
-        
-        Vector3 startPos = origin.position;
-        Vector3 direction = origin.forward;
-        Ray ray = new Ray(startPos, direction);
-        
+
+        Vector3 localStart = Vector3.zero; // start at origin
+        Vector3 localEnd = Vector3.forward * laserDistance; // default end in local space
+
+        // Raycast in world space
+        Ray ray = new Ray(origin.position, origin.forward);
         RaycastHit hit;
         bool hitSomething = Physics.Raycast(ray, out hit, laserDistance, ~ignoreMask);
-        
-        Vector3 endPos;
-        
+
         if (hitSomething)
         {
-            endPos = hit.point;
-            lineRenderer.SetPosition(0, startPos);
-            lineRenderer.SetPosition(1, endPos);
-            
+            Vector3 worldEnd = hit.point;
+            // Convert world hit point to local space relative to origin
+            localEnd = origin.InverseTransformPoint(worldEnd);
+
             AppleEnemy hitApple = hit.collider.GetComponentInParent<AppleEnemy>();
-            
+
             if (hitApple != null)
             {
                 if (currentTarget != hitApple)
                 {
-                    StopDamaging(ref currentTarget, ref isDamaging, particles);
+                    StopDamaging(ref currentTarget, ref isDamaging);
                     currentTarget = hitApple;
-                    StartDamaging(ref isDamaging, particles);
+                    StartDamaging(ref isDamaging);
                 }
-                
+
                 if (isDamaging && currentTarget != null)
                 {
                     currentTarget.TakeDamage(damage * Time.deltaTime);
@@ -136,46 +136,41 @@ public class LaserAttack : Attack
             }
             else
             {
-                StopDamaging(ref currentTarget, ref isDamaging, particles);
+                StopDamaging(ref currentTarget, ref isDamaging);
             }
         }
         else
         {
-            endPos = startPos + direction * laserDistance;
-            lineRenderer.SetPosition(0, startPos);
-            lineRenderer.SetPosition(1, endPos);
-            
-            StopDamaging(ref currentTarget, ref isDamaging, particles);
+            StopDamaging(ref currentTarget, ref isDamaging);
         }
-        
-        // Always position particles at laser endpoint
+
+        // Set LineRenderer in local space
+        lineRenderer.SetPosition(0, localStart);
+        lineRenderer.SetPosition(1, localEnd);
+
+        // Move particles to laser endpoint in world space
         if (particles != null)
         {
-            particles.transform.position = endPos;
+            particles.transform.position = origin.TransformPoint(localEnd);
+            
         }
     }
 
-    private void StartDamaging(ref bool isDamaging, ParticleSystem particles)
+    private void StartDamaging(ref bool isDamaging)
     {
         isDamaging = true;
-        
-        if (particles != null)
-        {
-            particles.Play();
-        }
+
+ 
     }
 
-    private void StopDamaging(ref AppleEnemy target, ref bool isDamaging, ParticleSystem particles)
+    private void StopDamaging(ref AppleEnemy target, ref bool isDamaging)
     {
         if (isDamaging)
         {
             isDamaging = false;
             target = null;
-            
-            if (particles != null)
-            {
-                particles.Stop();
-            }
+
+
         }
     }
 
